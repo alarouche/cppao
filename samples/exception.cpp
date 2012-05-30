81f7dbf977bfe81130d933a1e87c20c126ccd9f9
calum74 <calum74@gmail.com>
2012-05-30 16:00:12 +0000

    Work on samples.

---
+++ samples/exception.cpp
@@ -0,0 +1,41 @@
+#include <active_object.hpp>
+#include <iostream>
+
+struct ex_object : public active::object
+{
+	struct msg {};
+	
+	struct the_unthinkable : public std::runtime_error
+	{
+		the_unthinkable() : std::runtime_error("This could never happen") { }
+	};
+	
+	ACTIVE_METHOD(msg)
+	{
+		throw the_unthinkable();
+	}
+	
+	void exception_handler()
+	{
+		try
+		{
+			throw;
+		}
+		catch(the_unthinkable&)
+		{
+			std::clog << "The unthinkable has happened!\n";
+		}
+		catch(...)
+		{
+			// Invoke default handler
+			active::object::exception_handler();
+		}
+	}
+};
+
+int main()
+{
+	ex_object obj;
+	obj(ex_object::msg());
+	obj.run();
+}
