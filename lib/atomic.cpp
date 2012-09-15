ea4db100f2c9aa53cf5e17318ffb9f3b04f29ee8
calum74 <calum74@gmail.com>
2012-09-15 13:43:19 +0000

    fifo/lifo tweaks

--- lib/atomic.cpp
+++ lib/atomic.cpp
@@ -31,10 +31,10 @@ void active::atomic_fifo::push(atomic_node*n)
 	atomic_node * i;
 	do
 	{
-		i = input_queue.load(); // std::memory_order_relaxed);
+		i = input_queue.load( std::memory_order_relaxed);
 		n->next = i;
 	}
-	while( !input_queue.compare_exchange_weak( i, n ) ); //  , std::memory_order_relaxed) );
+	while( !input_queue.compare_exchange_weak( i, n, std::memory_order_relaxed) );
 }
 
 active::atomic_node * active::atomic_fifo::pop()
@@ -43,7 +43,7 @@ active::atomic_node * active::atomic_fifo::pop()
 	int spin_count=2000;
 	for(;;)
 	{
-		t=output_queue.load(); // std::memory_order_acquire);
+		t=output_queue.load( std::memory_order_acquire);
 		if( t==&busy )
 		{
 			if(!spin_count--)
@@ -55,9 +55,9 @@ active::atomic_node * active::atomic_fifo::pop()
 		}
 		else if( t==nullptr )
 		{
-			if( output_queue.compare_exchange_weak( t, &busy) ) // , std::memory_order_relaxed ) )
+			if( output_queue.compare_exchange_weak( t, &busy, std::memory_order_relaxed ) )
 			{
-				atomic_node * old_input = input_queue.exchange(nullptr); // , std::memory_order_relaxed);
+				atomic_node * old_input = input_queue.exchange(nullptr, std::memory_order_relaxed);
 				atomic_node * result = nullptr;
 				atomic_node * new_output = nullptr;
 				if(old_input!=nullptr)
@@ -75,21 +75,21 @@ active::atomic_node * active::atomic_fifo::pop()
 						else break;
 					}
 				}
-				output_queue.store( new_output ); // , std::memory_order_release );
+				output_queue.store( new_output, std::memory_order_release );
 				return result;
 			}
 		}
 		else
 		{
-			atomic_node * old_output = output_queue.exchange( &busy ); // , std::memory_order_relaxed );
+			atomic_node * old_output = output_queue.exchange( &busy, std::memory_order_relaxed );
 			
 			if( old_output != &busy )
 			{
 				atomic_node * result = old_output;
 				if(result)
-					output_queue.store( result->next ); // , std::memory_order_release );
+					output_queue.store( result->next, std::memory_order_release );
 				else
-					output_queue.store( nullptr ); // , std::memory_order_release );
+					output_queue.store( nullptr, std::memory_order_release );
 				return result;
 			}
 		}
@@ -103,7 +103,7 @@ void active::atomic_lifo::push(atomic_node * n)
 	atomic_node * l;
 	int spin_count=2000;
 	do {
-		l = list.load();
+		l = list.load(); // std::memory_order_relaxed);
 		if( l==&busy )
 		{
 			if( --spin_count==0)
@@ -114,26 +114,26 @@ void active::atomic_lifo::push(atomic_node * n)
 		}
 		else
 			n->next = l;
-	} while ( l==&busy || !list.compare_exchange_weak( l, n ) );
+	} while ( l==&busy || !list.compare_exchange_weak( l, n ) ); // , std::memory_order_relaxed ) );
 }
 
 active::atomic_node * active::atomic_lifo::pop()
 {
 	int spin_count=2000;
 	atomic_node * l;
-	do {
-		// Was: l=list.load();
-		l=list.exchange(&busy); // std::memory_order_acquire);
+	do
+	{
+		l=list.exchange(&busy);
 		if( l==&busy &&--spin_count==0 )
 		{
 			std::this_thread::sleep_for(std::chrono::seconds(0));
 			spin_count=2000;
 		}
-	} while ( l==&busy );  // Was: && !list.cas(l, &busy);
+	} while ( l==&busy );
 	
 	assert(l!=&busy);
 	
-	list.store(l ? l->next : nullptr); // , std::memory_order_release);
+	list.store(l ? l->next : nullptr); // , std::memory_order_relaxed);
 	return l;
 }
 
