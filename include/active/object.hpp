4826c4fb78ca540e5b727977e19cc05af93ee537
calum74 <calum74@gmail.com>
2012-09-16 17:43:04 +0000

    Variadic handlers

--- include/active/object.hpp
+++ include/active/object.hpp
@@ -619,7 +619,7 @@ namespace active
 	};
 
 #ifdef ACTIVE_USE_VARIADIC_TEMPLATES
-	template<typename Args...>
+    template<typename... Args>
 	struct sink
 	{
 		typedef platform::shared_ptr<sink<Args...> > sp;
@@ -627,32 +627,98 @@ namespace active
 		virtual void send(Args...)=0;
 	};
 
-	template<typename Derived, typename Args...>
+    template<typename Derived, typename... Args>
 	struct handle : public sink<Args...>
 	{
-		void send(Args... args) { static_cast<Derived&>(*this)(args); }
+        void send(Args... args) { static_cast<Derived&>(*this)(args...); }
 	};
 #else
 	template<typename A1=void, typename A2=void, typename A3=void,typename A4=void, typename A5=void>
-	struct sink;
+    struct sink
+    {
+        typedef platform::shared_ptr<sink<A1,A2,A3,A4,A5> > sp;
+        typedef platform::weak_ptr<sink<A1,A2,A3,A4,A5> > wp;
+        virtual void send(A1,A2,A3,A4,A5)=0;
+    };
+
 
 	template<typename Derived, typename A1=void, typename A2=void, typename A3=void,typename A4=void, typename A5=void>
-	struct handle;
+    struct handle : public sink<A1,A2,A3,A4,A5>
+    {
+        void send(A1 a1,A2 a2,A3 a3,A4 a4,A5 a5) { static_cast<Derived&>(*this)(a1,a2,a3,a4,a5); }
+    };
+
 	
-	template<typename T>
-	struct sink<T,void,void,void,void>
+    template<>
+    struct sink<>
+    {
+        typedef platform::shared_ptr<sink<> > sp;
+        typedef platform::weak_ptr<sink<> > wp;
+        virtual void send()=0;
+    };
+
+    template<typename A1>
+    struct sink<A1>
 	{
-		typedef platform::shared_ptr<sink<T> > sp;
-		typedef platform::weak_ptr<sink<T> > wp;
-		typedef T value_type;
-		virtual void send(value_type)=0;
+        typedef platform::shared_ptr<sink<A1> > sp;
+        typedef platform::weak_ptr<sink<A1> > wp;
+        virtual void send(A1)=0;
 	};
 
-	template<typename Derived, typename T>
-	struct handle<Derived,T,void,void,void,void> : public sink<T,void,void,void,void>
+    template<typename A1, typename A2>
+    struct sink<A1,A2>
+    {
+        typedef platform::shared_ptr<sink<A1,A2> > sp;
+        typedef platform::weak_ptr<sink<A1,A2> > wp;
+        virtual void send(A1,A2)=0;
+    };
+
+    template<typename A1, typename A2, typename A3>
+    struct sink<A1,A2,A3>
+    {
+        typedef platform::shared_ptr<sink<A1,A2,A3> > sp;
+        typedef platform::weak_ptr<sink<A1,A2,A3> > wp;
+        virtual void send(A1,A2,A3)=0;
+    };
+
+    template<typename A1, typename A2, typename A3, typename A4>
+    struct sink<A1,A2,A3,A4>
+    {
+        typedef platform::shared_ptr<sink<A1,A2,A3,A4> > sp;
+        typedef platform::weak_ptr<sink<A1,A2,A3,A4> > wp;
+        virtual void send(A1,A2,A3,A4)=0;
+    };
+
+    template<typename Derived>
+    struct handle<Derived> : public sink<>
+    {
+        void send() { static_cast<Derived&>(*this)(); }
+    };
+
+    template<typename Derived, typename A1>
+    struct handle<Derived,A1> : public sink<A1>
 	{
-		void send(T value) { static_cast<Derived&>(*this)(value); }
+        void send(A1 a1) { static_cast<Derived&>(*this)(a1); }
 	};
+
+    template<typename Derived, typename A1, typename A2>
+    struct handle<Derived,A1,A2> : public sink<A1,A2>
+    {
+        void send(A1 a1,A2 a2) { static_cast<Derived&>(*this)(a1,a2); }
+    };
+
+    template<typename Derived, typename A1, typename A2, typename A3>
+    struct handle<Derived,A1,A2,A3> : public sink<A1,A2,A3>
+    {
+        void send(A1 a1,A2 a2,A3 a3) { static_cast<Derived&>(*this)(a1,a2,a3); }
+    };
+
+    template<typename Derived, typename A1, typename A2, typename A3, typename A4>
+    struct handle<Derived,A1,A2,A3,A4> : public sink<A1,A2,A3,A4>
+    {
+        void send(A1 a1,A2 a2,A3 a3,A4 a4) { static_cast<Derived&>(*this)(a1,a2,a3,a4); }
+    };
+
 #endif
 	
 } // namespace active
